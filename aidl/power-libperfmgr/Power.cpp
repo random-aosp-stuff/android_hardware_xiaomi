8050ba4dbafbdef60119891ca195825301b3d885
Subhajeet Muhuri <subhajeet.muhuri@aosip.dev>
2020-12-03 12:59:23 +0530

    power-libperfmgr: Remove audio hints handling
    Signed-off-by: Subhajeet Muhuri <subhajeet.muhuri@aosip.dev>
    Change-Id: I79ae7d6a41e20c8ede50508e068458b609dd5720

--- aidl/power-libperfmgr/Power.cpp
+++ aidl/power-libperfmgr/Power.cpp
@@ -49,7 +49,6 @@ extern bool setDeviceSpecificMode(Mode type, bool enabled);
 #endif
 
 constexpr char kPowerHalStateProp[] = "vendor.powerhal.state";
-constexpr char kPowerHalAudioProp[] = "vendor.powerhal.audio";
 constexpr char kPowerHalRenderingProp[] = "vendor.powerhal.rendering";
 constexpr char kPowerHalAdpfRateProp[] = "vendor.powerhal.adpf.rate";
 constexpr int64_t kPowerHalAdpfRateDefault = -1;
@@ -70,12 +69,6 @@ Power::Power(std::shared_ptr<HintManager> hm)
         mSustainedPerfModeOn = true;
     } else {
         LOG(INFO) << "Initialize PowerHAL";
-    }
-
-    state = ::android::base::GetProperty(kPowerHalAudioProp, "");
-    if (state == "AUDIO_STREAMING_LOW_LATENCY") {
-        LOG(INFO) << "Initialize with AUDIO_LOW_LATENCY on";
-        mHintManager->DoHint(state);
     }
 
     state = ::android::base::GetProperty(kPowerHalRenderingProp, "");
@@ -128,8 +121,6 @@ ndk::ScopedAStatus Power::setMode(Mode type, bool enabled) {
             [[fallthrough]];
         case Mode::DISPLAY_INACTIVE:
             [[fallthrough]];
-        case Mode::AUDIO_STREAMING_LOW_LATENCY:
-            [[fallthrough]];
         default:
             if (enabled) {
                 mHintManager->DoHint(toString(type));
@@ -173,8 +164,6 @@ ndk::ScopedAStatus Power::setBoost(Boost type, int32_t durationMs) {
         case Boost::DISPLAY_UPDATE_IMMINENT:
             [[fallthrough]];
         case Boost::ML_ACC:
-            [[fallthrough]];
-        case Boost::AUDIO_LAUNCH:
             [[fallthrough]];
         default:
             if (mSustainedPerfModeOn) {
